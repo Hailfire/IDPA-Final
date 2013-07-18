913474a930cbcf1e073cac4528cf632d9be8230d
Hailfire <ashamshad96@gmail.com>
2013-07-18 15:45:14 -0500

    added first battle sequence
    learned something new

--- TBA/src/Driver.java
+++ TBA/src/Driver.java
@@ -1,14 +1,16 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Driver
 {
 
-		/**
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
-		Player man = new Player();
+		Random rand = new Random();
+		Player person = new Player();
 
 		System.out.println("You see two paths before you one leads to a castle the other " + "takes you to a small area with a few surprises. "
 				+ "Path 1 or path 2");
@@ -24,30 +26,37 @@ public class Driver
 			if (selection.equals("a"))
 			{
 				System.out.println("the orc is killed at the cost of 100 lifepoints");
-				man.Health -= 100;
-				System.out.println("Your health is now " + man.Health);
-			   man.money += 2825;
-                System.out.println("Your money is now" + man.money); 
+				person.Health -= 100;
+				System.out.println("Your health is now " + person.Health);
+				person.money += 2825;
+				System.out.println("Your money is now" + person.money);
+
+			}
 			
-			}    
-                {
-			 System.out.println("you come across a full health potion and recieve full health, you arrive at the entrace to the castle but you also see a dungeon where wi.l you go, both hold unknown surprises and dangers d, dungeon or c castle");
-			 man.Health += 100;
-			 System.out.println("Your health is now " + man.Health);
-			 man.lvl += 2;
-			 System.out.println("Your lvl is now" + man.lvl);
-			}
-			  
-			{
-			selection = sc.nextLine(); 
-		    if (selection.equals("d"));
-			}
-        
-			{
-			  System.out.println("You go down the hole and into the dungeon where a troll with 100 base damge and 35 dark damage awaits you, ");
+			System.out
+					.println("you come across a full health potion and recieve full health, you arrive at the" +
+							" entrace to the castle but you also see a dungeon where wi.l you go, both hold unknown " +
+							"surprises and dangers d, dungeon or c castle");
+			person.Health += 100;
+			System.out.println("Your health is now " + person.Health);
+			person.lvl += 2;
+			System.out.println("Your lvl is now" + person.lvl);
 			
+			if (selection.equals("d"))
+			{
+				System.out
+						.println("You go down the hole and into the dungeon where a troll with 100 base damge and 35 dark damage awaits you, do you battle or flee");
 			}
-	   	
+
+
+			selection = sc.nextLine();
+			if (selection.equals("run") || selection.equals("b"))
+			{
+				System.out.println("Your cowardice has cost you 120 hp, well done(sarcasm)");
+				person.Health -= 120;
+				System.out.println("Your health is now " + person.Health);
+			}
+
 		}
 		else if (selection.equals("path 2") || selection.equals("2"))
 		{
@@ -61,16 +70,46 @@ public class Driver
 				selection = sc.nextLine();
 				if (selection.equals("fight"))
 				{
-					
-				System.out.println("the bog giant is killed with no damage taken but it now evolves into a moss golem(scary!!!!) do you a, confront it or b, piss your pants and run away");
-				man.money += 3000;             
-				System.out.println("Your money is now" + man.money);             
-				
-            }
 
+					System.out.println("the bog giant is killed with no damage taken but it now evolves into a moss golem(scary!!!!) " +
+							"do you a, confront it or b, piss your pants and run away");
+					person.money += 3000;
+					System.out.println("Your money is now" + person.money);
+
+				}
+
+				selection = sc.nextLine();
+				if (selection.equals("confront"))
+				{
+					int golemHealth = 200;
+					while(person.Health > 0 && golemHealth > 0)
+					{
+						System.out.println("1. attack ; 2. defend ");
+						int sel = sc.nextInt();
+						int golemAttack = rand.nextInt(21);
+						if(sel == 1)
+						{
+							int randAttack = rand.nextInt(person.attack);
+							golemHealth -= randAttack;
+							System.out.println("You attack the golem for " + randAttack + " damage");
+						}
+						else if (sel == 2)
+						{
+							golemAttack /= 2;
+							System.out.println("You defend");	
+						}
+						
+						person.Health -= golemAttack;
+						System.out.println("golem attacks you for " + golemAttack + " damage");
+						System.out.println("Your Health is now" + person.Health);
+					}
+					
+				}
+				person.lvl += 4;
+
+			}
 		}
+
 	}
-	
-	}		
-	
-}			
+
+}
